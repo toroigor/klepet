de5fa8a19f097ba439df435533caf928c419f118
Igor Todorovic <it9026@student.uni-lj.si>
2016-04-10 19:44:28 +0000

    Zaključek dela na I. nalogi

--- public/js/klepet_UI.js
+++ public/js/klepet_UI.js
@@ -99,7 +99,13 @@ $(document).ready(function() {
     for (var i=0; i < uporabniki.length; i++) {
       $('#seznam-uporabnikov').append(divElementEnostavniTekst(uporabniki[i]));
     }
+    
+    $('#seznam-uporabnikov div').click(function() {
+      klepetApp.procesirajUkaz('/zasebno ' + $(this).text());
+      $('#poslji-sporocilo').focus();
+    });
   });
+
 
   setInterval(function() {
     socket.emit('kanali');
